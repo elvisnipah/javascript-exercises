4041e0f3492ee180ad918e1394c1150d3832c037
Elvis <elvisnipah@gmail.com>
2022-03-30 17:41:54 +0000

    Run code tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    let i = 0;
+    while (i < num) {
+        finalString += string;
+        i++;
+    }
+    if (num < 0) {
+        finalString += "ERROR";
+    };
+    return finalString;
 };
 
 // Do not edit below this line
