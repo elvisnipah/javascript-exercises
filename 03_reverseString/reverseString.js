ee62106cf5375e75d1b0a36eded06c53fdefb757
Elvis <elvisnipah@gmail.com>
2022-03-30 18:03:29 +0000

    Add code to pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const splitString = str.split("");
+    splitString.reverse();
+    let newString = splitString.join("");
+    return newString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
