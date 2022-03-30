09eb6b0fdc404988a3267ec986ca0587bc367fb9
Elvis <elvisnipah@gmail.com>
2022-03-30 19:01:51 +0000

    Add code to pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...selection) {
+    for (let i = 0; i < selection.length; i ++){
+        const index = arr.indexOf(selection[i]);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
