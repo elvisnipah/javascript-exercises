b79fb30f45e342a424657af2d1b4438267ed5c9f
Elvis <elvisnipah@gmail.com>
2022-04-04 15:24:27 +0000

    Add code to pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let str = text.toLowerCase();
+    str = str.replace(/[^a-zA-Z0-9]/g, '');
+    let splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    if(joinArray === str) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
