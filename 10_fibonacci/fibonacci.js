9c39038dfd65ad924b79bb4d900e9a07651bddd0
Elvis <elvisnipah@gmail.com>
2022-04-05 07:51:45 +0000

    Add code to pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num >= 0) {
+        let realNum = parseInt(num);
+        let n1 = 1, n2 = 1, nextTerm;
 
+        for(i = 1; i <= realNum - 1; i++) {
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
+        return n1;
+    }
+
+    else {
+        let error = "OOPS";
+        return error;
+    }
 };
 
 // Do not edit below this line
